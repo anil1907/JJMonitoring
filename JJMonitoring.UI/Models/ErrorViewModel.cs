98bb04dadb8d998f5e063ee8de337b2f3ddc062b
Anıl YILDIRIM <anil.yildirim@procat.com.tr>
2021-10-27 12:33:20 +0300

    Add project files.

---
+++ JJMonitoring.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JJMonitoring.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
